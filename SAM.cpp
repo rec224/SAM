59d9fc36ada477bc3577b2d51edc8dd651c89215
rec224 <rec224@lehigh.edu>
2023-02-27 12:47:08 -0500

    looking at these eigen dirs

--- SAM.cpp
+++ SAM.cpp
@@ -1,7 +1,7 @@
 int N;
 int matrix_mult(int[][16], int[][16], int[][16]);
 int SAM();
-int matrix_mult(int **a, int **b, int **c){
+int matrix_mult(int [][16] a, int [][16]b, int [][16]c){
     //function matrix_mult takes two full matrices and an empty matrix
     //the function stores the multiplication in the new matrix
     //return 0 if there is a successful multiplication
@@ -20,8 +20,9 @@ int SAM(){
     //here we are saying we want the size of dimension 1 in A
     int N = sizeof(A[0]);
     //DSR1 = spdiags(1./sqrt(diag(A), [0], N, N)
-    //As = Dsr1*(A*Dsr1)
+    //spdiags (A, d,m, n ) takes the cols of A and places on diagonals specified by d
     
+    //As = Dsr1*(A*Dsr1)
     int multSuccess = matrix_mult(A, B, C);
     return 0;
 }
